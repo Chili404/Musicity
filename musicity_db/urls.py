a8ea3a6efc48d625a5329959708c51319ee0ee3b
Chili404 <kamalchill1278@gmail.com>
2022-10-29 20:25:39 -0400

    Created URL's
    Implemented URL's that work for Different CRUD operations

--- musicity_db/urls.py
+++ musicity_db/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('admin/', views.track_form), #localhost:p#/employee/list
+    path('', views.track_form), #localhost:p#/employee/list
     path('list/', views.track_list)
 ]
--- musicity_db/views.py
+++ musicity_db/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def track_list(request):
-    return
+    return render(request, "musicity_db/track_list.html")
 
 def track_form(request):
-    return
+    return render(request, "musicity_db/track_form.html")
 
 def track_delete(request):
     return
