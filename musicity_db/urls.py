735563625535d541017fe6da1bee2490406215a1
Chili404 <kamalchill1278@gmail.com>
2022-10-29 22:34:09 -0400

    Added edit and remove functionality

--- musicity_db/urls.py
+++ musicity_db/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('track/', views.track_list), #localhost:p#/employee/list
-    path('track/form/', views.track_form)
+    path('track/', views.track_list, name="track_list"), #localhost:p#/employee/list
+    path('track/form/', views.track_form, name="track_insert"),
+    path('track/<int:id>/', views.track_form, name="track_update"),
+    path('track/delete/<int:id>', views.track_delete, name="track_delete")
 ]
--- musicity_db/views.py
+++ musicity_db/views.py
@@ -6,15 +6,25 @@ def track_list(request):
     context = {'track_list':Track.objects.all()}
     return render(request, "musicity_db/track_list.html", context)
 
-def track_form(request):
+def track_form(request, id=0):
     if request.method == "GET":
-        form = TrackForm()
+        if id == 0: #insert
+            form = TrackForm()
+        else: #update
+            track = Track.objects.get(pk=id)
+            form = TrackForm(instance=track)          
         return render(request, "musicity_db/track_form.html", {'form':form})
     else:
-        form = TrackForm(request.POST)
+        if id == 0:
+            form = TrackForm(request.POST)
+        else:
+            track = Track.objects.get(pk=id)
+            form = TrackForm(request.POST, instance = track)
         if form.is_valid():
             form.save()
-        return redirect('musicity/track/')
+        return redirect('/musicity/track/')
 
-def track_delete(request):
-    return
+def track_delete(request, id):
+    track = Track.objects.get(pk=id)
+    track.delete()
+    return redirect('/musicity/track/')
