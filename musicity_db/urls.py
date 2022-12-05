1efc79be49ece804a2f3f2d6b53309757fb8946d
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2022-12-04 23:05:10 -0500

    Added artist table, form , and list

--- musicity_db/urls.py
+++ musicity_db/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path,include
+from django.urls import path, include
 from . import views
 urlpatterns = [
     path('home/', views.home, name="home"),
 
     # track urls
-    path('track/', views.track_list, name="track_list"), #localhost:p#/employee/list
+    # localhost:p#/employee/list
+    path('track/', views.track_list, name="track_list"),
     path('track/form/', views.track_form, name="track_insert"),
     path('track/<int:id>/', views.track_form, name="track_update"),
     path('track/delete/<int:id>', views.track_delete, name="track_delete"),
@@ -17,5 +18,13 @@ urlpatterns = [
     path('album/<int:id>/', views.album_form, name="album_update"),
     path('album/delete/<int:id>/', views.album_delete, name="album_delete"),
     path('album/<str:header>', views.album_sort_asc, name="album_sort_asc"),
-    path('album/<str:header>', views.album_sort_dec, name="album_sort_dec")
+    path('album/<str:header>', views.album_sort_dec, name="album_sort_dec"),
+
+    # artist urls
+    path('artist/', views.artist_list, name="artist_list"),
+    path('artist/form/', views.artist_form, name="artist_insert"),
+    path('artist/<int:id>/', views.artist_form, name="artist_update"),
+    path('artist/delete/<int:id>/', views.artist_delete, name="artist_delete"),
+    path('artist/<str:header>', views.artist_sort_asc, name="artist_sort_asc"),
+    path('artist/<str:header>', views.artist_sort_dec, name="artist_sort_dec")
 ]
